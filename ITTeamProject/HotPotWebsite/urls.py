7e7a871884f70520b28a292750e1e22cb12eeb76
Teng Li <2344067l@student.gla.ac.uk>
2018-03-14 21:58:43 +0000

    mapping urls

--- ITTeamProject/HotPotWebsite/urls.py
+++ ITTeamProject/HotPotWebsite/urls.py
@@ -4,4 +4,8 @@ from HotPotWebsite import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^about', views.about, name = 'about'),
+    url(r'^message_board/$', views.message_board, name='message_board'),
+    url(r'^book/$', views.book, name='book'),
+    url(r'^menu/$', views.menu, name='menu'),
 ]
--- ITTeamProject/HotPotWebsite/views.py
+++ ITTeamProject/HotPotWebsite/views.py
@@ -8,3 +8,15 @@ def index(request):
     # Note that the first parameter is the template we wish to use
     response = render(request, 'HotPotWebsite/index.html')
     return response
+
+def message_board(request):
+    return render(request, 'HotPotWebsite/message_board.html')
+
+def book(request):
+    return render(request, 'HotPotWebsite/message_board.html')
+
+def menu(request):
+    return render(request, 'HotPotWebsite/message_board.html')
+
+def about(request):
+    return render(request, 'HotPotWebsite/about.html')
